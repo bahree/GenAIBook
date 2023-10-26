e32730f0d42f526b9fb8bb088911885230ee13fd
Amit Bahree <github@desigeek.com>
2023-10-25 22:34:12 -0700

    Updates to Chapter 1 and 3 based on feedback

--- chapters/ch01/Listing-1.1-helloworld.py
+++ chapters/ch01/Listing-1.1-helloworld.py
@@ -1,14 +1,15 @@
 # Listing: 1.1 - Basic Hello World example using the OpenAI API
-
 import openai
 
+gpt_model = "text-davinci-003"
+
 # Replace with your actual OpenAI API key and organization ID
-openai.api_key = 'your-api-key'  
+openai.api_key = 'your-api-key'
 openai.organization = "your-organization-id-here"
 
 # Generate English text
 response_english = openai.Completion.create(
-  engine="text-davinci-003",
+  engine=gpt_model,
   prompt="Hello, World!",
   max_tokens=50
 )
@@ -17,7 +18,7 @@ print(response_english.choices[0].text.strip())  # This prints the completion of
 
 # Translate English text to French
 response_french = openai.Completion.create(
-  engine="text-davinci-003",
+  engine=gpt_model,
   prompt=f'Translate the following English text to French: "{response_english.choices[0].text.strip()}"',
   max_tokens=60
 )
--- chapters/ch03/Listing-3.21-petchat.py
+++ chapters/ch03/Listing-3.21-petchat.py
@@ -6,7 +6,7 @@ openai.api_base = os.getenv("AOAI_ENDPOINT")
 openai.api_version = "2023-05-15"
 openai.api_key = os.getenv("AOAI_KEY")
 
-conversation=[{"role": "system", "content": "You are a helpful AI assistant and happt to talk about pets and salons."}]
+conversation=[{"role": "system", "content": "You are a helpful AI assistant and happy to talk about pets and salons."}]
 
 while True:
     user_input = input()      
